8d3166570b68819e0866e089d1a3d1fb8151831d
kdsalvy <kedia.saurabh@gmail.com>
2020-05-08 18:27:47 +0530

    Added Jpa Rest Repo to fetch data using api

--- spring-boot-flyway-example/src/main/java/kd/poc/flyway/domain/UserRepository.java
+++ spring-boot-flyway-example/src/main/java/kd/poc/flyway/domain/UserRepository.java
@@ -1,10 +1,10 @@
 package kd.poc.flyway.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  */
-@Repository
+@RepositoryRestResource(path = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
 }
